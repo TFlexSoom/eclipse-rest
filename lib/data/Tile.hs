37bec4e58895f126cf32d04b72f072b396663836
TFlexSoom <tchilbert@live.com>
2023-02-11 23:26:41 -0800

    Fix Up Tile

--- lib/data/Tile.hs
+++ lib/data/Tile.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE DuplicateRecordFields #-}
 module Tile (
+  Rotation,
+  TileDegree(..),
+  PlanetSlot(..),
+  Tile(..)
   TileMap(..),
-  TileMapImpl,
-  newTileMap,
-
+  newTileMap
 ) where
 
 import Data.Misc ( UniqueId(..), PlanetResourceType, Building )
@@ -20,15 +22,14 @@ data PlanetSlot = PlanetSlot {
   isOrbital :: Boolean
 }
 
-data Neighbor = NO_GATE | EMPTY | CONNECTED UniqueId
-
 data Tile {
   id :: UniqueId,
   disk :: Maybe PlayerId,
+  coordinate :: (Int, Int),
   slots :: [PlanetSlot],
   ships :: [Ship],
   buildings :: [Building],
-  neighbors :: [Neighbor]
+  gates :: [Boolean]
 }
 
 class TileMap a where
@@ -36,15 +37,15 @@ class TileMap a where
   getCenter :: a -> Tile
   getHomeSystem :: a -> PlayerId -> Tile
   getTile :: a -> UniqueId -> Tile
-  getNeighbors :: a -> Tile -> [Maybe Tile]
-  getNeighborsFromId :: a -> UniqueId -> [Maybe Tile]
+  getTileAt :: a -> (Int, Int) -> Maybe Tile
   getAllTiles :: a -> [Tile]
 
   drawTile :: a -> TileDegree -> (a, Tile)
-  placeTile :: a -> (Tile, Rotation) -> (Int, Rotation) -> a
+  placeTile :: a -> (Tile, Rotation) -> (Int, Int) -> a
 
 data TileMapImpl = TileMapImpl {
   stacks :: Map TileDegree [Tile]
+  placements :: Map (Int, Int) UniqueId
   graph :: Map UniqueId Tile
   center :: Tile,
   homeSystems :: Map PlayerId Tile
@@ -54,10 +55,9 @@ instance TileMap TileMapImpl where
   getStackCount TileMapImpl { stacks = stacks } degree = length ((!) stacks degree)
   getCenter TileMapImpl { center = center } = center
   getHomeSystem TileMapImpl { homeSystems = homeSystems } pid = (!) homeSystems pid
-  getTile TileMapImpl {graph = graph} uid = fst((!) graph uid )
-  getNeighbors self tile = getNeighborsFromId self ((id :: Tile -> UniqueId) tile)
-  getNeighborsFromId TileMapImpl {graph = graph} uid = map getTile (snd((!) graph uid))
-  getAllTiles TileMapImpl {graph = graph} = map fst (elems graph)
+  getTile TileMapImpl { graph = graph } uid = (!) graph uid
+  getTileAt TileMapImple { placements = placements } coord = lookup coord placements
+  getAllTiles TileMapImpl { graph = graph } = elems graph
 
   drawTile tileMap@TileMapImpl {stacks = stacks} deg = (altered, head degreeStack)
     where 
@@ -65,10 +65,24 @@ instance TileMap TileMapImpl where
       altered = tileMap{stacks= insert deg (tail degreeStack) stacks}
   
   -- TODO do complex logic to unstub this
-  placeTile tileMap (t, offset) (distance, angle) = tileMap
+  placeTile tileMap (t, offset) (distance, angle) = error "This Function is not Implemented!"
 
-newTileMap :: TileMapImpl
-newTileMap = TileMapImpl {}
+newTileMap :: TileMap a => a
+newTileMap = TileMapImpl {
+  stacks = empty, -- TODO Gen from Rules
+  placements = empty, -- TODO Needs HomeSystems + Placements
+  graph = empty, -- TODO Needs HomeSystems + Placements
+  center = newTile -- TODO Gen from Rules
+  homeSystems = empty -- TODO Gen from Rules?
+}
 
 newTile :: Tile
-newTile = Tile {}
+newTile = Tile {
+  id = 0, -- TODO Gen from Rules
+  disk = Nothing,
+  coordinate = (0, 0),
+  slots = [], -- TODO Gen From Rules
+  ships = [],
+  buildings = [],
+  gates = [] -- TODO Gen From Rules
+}
